1d650ecb09f4138159165f8568408962583d598c
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2023-02-04 10:42:06 +0545

    setting up the app using chat engine API to build serverless chat

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {ChatEngine}from 'react-chat-engine';
 
 function App() {
   return (
-    <div className="App">
-< h1>Hello World</h1>   
- </div>
+   <ChatEngine
+      height='95vh'
+			projectID='2348860e-4bd5-4403-922d-c0ed5cb8ae3c'
+			userName='Rocky'
+			userSecret='rocky123'
+		/>
+    
   );
 }
 
